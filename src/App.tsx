72f684033196aa10eddbb71c9e89763977be2cb8
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2024-03-28 12:10:57 -0500

    feat: iniciar proyecto

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="bg-green-500	">
+        <h1>Hola mundo</h1>
+      </div>
+      <div className="card">
+        <button>
+          count
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
